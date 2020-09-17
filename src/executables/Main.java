4556f64995829d809efebe51bacf24f0c29d9b46
josealvarocoelho <jose.alvaro.coelho@gmail.com>
2020-09-17 04:29:31 +0100

    Created populate class to populate the world with objects.

--- src/executables/Main.java
+++ src/executables/Main.java
@@ -1,18 +1,10 @@
 package executables;
 
-import activities.Exercise;
-
-
-
 public class Main {
 
+	
 	public static void main(String[] args) {
-		Exercise exercise1 = new Exercise(5, 120, "C Scale", "Book", Exercise.Type.FINGERSTYLE);
-		System.out.println(exercise1.getDuration());
-		System.out.println(exercise1.getBpm());
-		System.out.println(exercise1.getName());
-		System.out.println(exercise1.getSource());
-		System.out.println(exercise1.type);
+		Populate.populate();
 	}
 
 }
